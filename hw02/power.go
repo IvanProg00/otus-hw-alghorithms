d5556c195766fcca86dc7b218222d605e3f781b0
Ivan Gavrilov <gavrilovivan2000@gmail.com>
2022-03-13 11:43:26 +0100

    Implement iterative power

---
+++ hw02/power.go
@@ -0,0 +1,11 @@
+package power
+
+func IterPower(x float64, y int) float64 {
+	var res float64 = 1
+
+	for i := 0; i < y; i++ {
+		res *= x
+	}
+
+	return res
+}
---
+++ hw02/power_test.go
@@ -0,0 +1,39 @@
+package power
+
+import (
+	"fmt"
+	"math"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestIterPower(t *testing.T) {
+	const roundUntil = 100000000000
+	tests := []struct {
+		x        float64
+		y        int
+		expected float64
+	}{
+		{2, 10, 1024},                              // 0.00s
+		{123456789, 0, 1},                          // 0.00s
+		{1.001, 1000, 2.71692393224},               // 0.00s
+		{1.0001, 10000, 2.71814592682},             // 0.00s
+		{1.00001, 100000, 2.71826823719},           // 0.00s
+		{1.000001, 1000000, 2.7182804691},          // 0.00s
+		{1.0000001, 10000000, 2.71828169413},       // 0.03s
+		{1.00000001, 100000000, 2.71828179835},     // 0.14s
+		{1.000000001, 1000000000, 2.71828205201},   // 1.25s
+		{1.0000000001, 10000000000, 2.71828205323}, // 12.53s
+	}
+
+	for i, tt := range tests {
+		t.Run(fmt.Sprintf("test-%d", i), func(t *testing.T) {
+			require := require.New(t)
+
+			res := IterPower(tt.x, tt.y)
+			res = math.Round(res*roundUntil) / roundUntil
+			require.Equal(tt.expected, res)
+		})
+	}
+}
