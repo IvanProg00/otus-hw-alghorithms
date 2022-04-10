d98ba401a776faaa859448cd7f05c570cfffc4b8
Ivan Gavrilov <gavrilovivan2000@gmail.com>
2022-04-10 12:52:10 +0200

    Add Shell sort

--- hw05/sort_test.go
+++ hw05/sort_test.go
@@ -76,3 +76,38 @@ func TestInsertionSort(t *testing.T) {
 		})
 	}
 }
+
+func TestShellSort(t *testing.T) {
+	tests := []struct {
+		val      []int
+		expected []int
+	}{
+		{
+			val:      []int{},
+			expected: []int{},
+		},
+		{
+			val:      []int{5, 0},
+			expected: []int{0, 5},
+		},
+		{
+			val:      []int{1, 2, 4},
+			expected: []int{1, 2, 4},
+		},
+		{
+			val:      []int{7, 3, 8},
+			expected: []int{3, 7, 8},
+		},
+		{
+			val:      []int{8, 43, 984, 1, 43, 64},
+			expected: []int{1, 8, 43, 43, 64, 984},
+		},
+	}
+
+	for i, tt := range tests {
+		t.Run(fmt.Sprintf("test-%d", i), func(t *testing.T) {
+			ShellSort(tt.val)
+			require.EqualValues(t, tt.expected, tt.val)
+		})
+	}
+}
