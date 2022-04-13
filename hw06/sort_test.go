767e10d46d1589d35ca4cd65dadfac257e135e91
Ivan Gavrilov <gavrilovivan2000@gmail.com>
2022-04-13 19:31:52 +0200

    Implement Selection sort

---
+++ hw06/sort_test.go
@@ -0,0 +1,43 @@
+package sort
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestSelectionSort(t *testing.T) {
+	tests := []struct {
+		val      []int
+		expected []int
+	}{
+		{
+			val:      []int{},
+			expected: []int{},
+		},
+		{
+			val:      []int{5, 0},
+			expected: []int{0, 5},
+		},
+		{
+			val:      []int{1, 2, 4},
+			expected: []int{1, 2, 4},
+		},
+		{
+			val:      []int{7, 3, 8},
+			expected: []int{3, 7, 8},
+		},
+		{
+			val:      []int{8, 43, 984, 1, 43, 64},
+			expected: []int{1, 8, 43, 43, 64, 984},
+		},
+	}
+
+	for i, tt := range tests {
+		t.Run(fmt.Sprintf("test-%d", i), func(t *testing.T) {
+			SelectionSort(tt.val)
+			require.EqualValues(t, tt.expected, tt.val)
+		})
+	}
+}
